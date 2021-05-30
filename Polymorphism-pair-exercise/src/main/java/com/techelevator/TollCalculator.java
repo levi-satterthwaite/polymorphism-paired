7825e025beb6221e30fcf86afd4352db9ee1f911
Levi Satterthwaite <levi.satt001@gmail.com>
2021-05-30 13:13:26 -0400

    almost done with toll calculator

---
+++ Polymorphism-pair-exercise/src/main/java/com/techelevator/TollCalculator.java
@@ -0,0 +1,21 @@
+package com.techelevator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TollCalculator {
+    public static void main(String[] args) {
+
+    List<Vehicle> vehiclesThroughToll = new ArrayList<Vehicle>();
+    vehiclesThroughToll.add( new Car() );
+    vehiclesThroughToll.add( new Car(true) );
+    vehiclesThroughToll.add( new Tank() );
+    vehiclesThroughToll.add( new Truck(4) );
+    vehiclesThroughToll.add( new Truck(6) );
+    vehiclesThroughToll.add( new Truck(8) );
+
+    System.out.printf()
+
+    }
+
+}
